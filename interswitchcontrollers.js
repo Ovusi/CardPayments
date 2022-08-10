1affd45b478ad02bc1b108be845bea6e16ccf037
Nobert Jakpor <jakpornobert@gmail.com>
2022-08-10 16:21:35 +0100

    new

--- interswitchcontrollers.js
+++ interswitchcontrollers.js
@@ -1,9 +1,9 @@
 const axios = require("axios")
 
-const paymentflow = async () => {
+const paymentflow = async (amount, token) => {
     const params = `{
         "customerId": "1407002510",
-        "amount": "20000",
+        "amount": ${amount},
         "transactionRef": "12n345mmm0km655",
         "currency": "NGN",
         "authData": "G3cf/VTtAHCdHZNxc5GXWRI8z5P0goL2amXWDVFgb6D3XK/QMtZW90TYdl5zffDCNpiZThJzk0+eEU/Y/aYS6fyIOpQZGFrOr8hmvx5869sl2kr5u8qjnM7q5b4ZnTqdKDLtNxr3Qr7anj6YLpox1FOsiyT26mktXL+7SFOaZ15NMtne1z4xrj4R2SndowI/Znsapo7Gfzvp+L7XJyQ8kLYYRk3INjvmRPPQoJg1R0Nnh6EQE3ldIdwylB7GKtr6a71N/yCd4ZtyIcqq1ZNzdWcZyy5eEBAlDIxuECdBqH6hRq2/RbkfARqidNN4Kq0WviSRaRYGbiNjl2W9pNcM8g=="
@@ -14,7 +14,7 @@ const paymentflow = async () => {
       path: '/api/v3/purchases',
       method: 'POST',
       headers: {
-        Authorization: 'Bearer <token>',
+        Authorization: `Bearer ${token}`,
         'Content-Type': 'application/json'
       }
     };
@@ -34,4 +34,27 @@ const paymentflow = async () => {
     });
     request.write(params);
     request.end();
+}
+
+const paymentsflow = async (amount, token) => {
+    const params = `{
+        "customerId": "1407002510",
+        "amount": ${amount},
+        "transactionRef": "12n345mmm0km655",
+        "currency": "NGN",
+        "authData": "G3cf/VTtAHCdHZNxc5GXWRI8z5P0goL2amXWDVFgb6D3XK/QMtZW90TYdl5zffDCNpiZThJzk0+eEU/Y/aYS6fyIOpQZGFrOr8hmvx5869sl2kr5u8qjnM7q5b4ZnTqdKDLtNxr3Qr7anj6YLpox1FOsiyT26mktXL+7SFOaZ15NMtne1z4xrj4R2SndowI/Znsapo7Gfzvp+L7XJyQ8kLYYRk3INjvmRPPQoJg1R0Nnh6EQE3ldIdwylB7GKtr6a71N/yCd4ZtyIcqq1ZNzdWcZyy5eEBAlDIxuECdBqH6hRq2/RbkfARqidNN4Kq0WviSRaRYGbiNjl2W9pNcM8g=="
+    }`;
+    const options = {
+      baseURL: 'https://payment-service.k8.isw.la/api/v3/purchases',
+      port: 443,
+      path: '/api/v3/purchases',
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json'
+      }
+    };
+    
+    
+    
 }
