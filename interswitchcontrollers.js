a6fbb77352c20b97799a7e32399917872fd384ed
Nobert Jakpor <jakpornobert@gmail.com>
2022-08-12 10:51:57 +0100

    new

--- interswitchcontrollers.js
+++ interswitchcontrollers.js
@@ -1,60 +1,72 @@
 const axios = require("axios")
 
 const paymentflow = async (amount, token) => {
-    const params = `{
+  const params = `{
         "customerId": "1407002510",
         "amount": ${amount},
         "transactionRef": "12n345mmm0km655",
         "currency": "NGN",
         "authData": "G3cf/VTtAHCdHZNxc5GXWRI8z5P0goL2amXWDVFgb6D3XK/QMtZW90TYdl5zffDCNpiZThJzk0+eEU/Y/aYS6fyIOpQZGFrOr8hmvx5869sl2kr5u8qjnM7q5b4ZnTqdKDLtNxr3Qr7anj6YLpox1FOsiyT26mktXL+7SFOaZ15NMtne1z4xrj4R2SndowI/Znsapo7Gfzvp+L7XJyQ8kLYYRk3INjvmRPPQoJg1R0Nnh6EQE3ldIdwylB7GKtr6a71N/yCd4ZtyIcqq1ZNzdWcZyy5eEBAlDIxuECdBqH6hRq2/RbkfARqidNN4Kq0WviSRaRYGbiNjl2W9pNcM8g=="
     }`;
-    const options = {
-      hostname: 'https://payment-service.k8.isw.la',
-      port: 443,
-      path: '/api/v3/purchases',
-      method: 'POST',
-      headers: {
-        Authorization: `Bearer ${token}`,
-        'Content-Type': 'application/json'
-      }
-    };
-    
-    
-    const request = https.request(options, response => {
-      response.on('data', (chunk) => {
-        data += chunk;
-      })
-    
-      response.on('end', () => {
-        console.log(JSON.parse(data));
-      });
-    
-    }).on('error', error => {
-      console.log('error: ', error);
+  const options = {
+    hostname: 'https://payment-service.k8.isw.la',
+    port: 443,
+    path: '/api/v3/purchases',
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json'
+    }
+  };
+
+
+  const request = https.request(options, response => {
+    response.on('data', (chunk) => {
+      data += chunk;
+    })
+
+    response.on('end', () => {
+      console.log(JSON.parse(data));
     });
-    request.write(params);
-    request.end();
+
+  }).on('error', error => {
+    console.log('error: ', error);
+  });
+  request.write(params);
+  request.end();
+
 }
 
 const paymentsflow = async (amount, token) => {
-    const params = `{
-        "customerId": "1407002510",
-        "amount": ${amount},
-        "transactionRef": "12n345mmm0km655",
-        "currency": "NGN",
-        "authData": "G3cf/VTtAHCdHZNxc5GXWRI8z5P0goL2amXWDVFgb6D3XK/QMtZW90TYdl5zffDCNpiZThJzk0+eEU/Y/aYS6fyIOpQZGFrOr8hmvx5869sl2kr5u8qjnM7q5b4ZnTqdKDLtNxr3Qr7anj6YLpox1FOsiyT26mktXL+7SFOaZ15NMtne1z4xrj4R2SndowI/Znsapo7Gfzvp+L7XJyQ8kLYYRk3INjvmRPPQoJg1R0Nnh6EQE3ldIdwylB7GKtr6a71N/yCd4ZtyIcqq1ZNzdWcZyy5eEBAlDIxuECdBqH6hRq2/RbkfARqidNN4Kq0WviSRaRYGbiNjl2W9pNcM8g=="
-    }`;
-    const options = {
-      baseURL: 'https://payment-service.k8.isw.la/api/v3/purchases',
-      port: 443,
-      path: '/api/v3/purchases',
-      method: 'POST',
-      headers: {
-        Authorization: `Bearer ${token}`,
-        'Content-Type': 'application/json'
-      }
-    };
-    
-    
-    
+  const param = {
+    customerId: "1407002510",
+    amount: amount,
+    transactionRef: "12n345mmm0km655",
+    currency: "NGN",
+    authData: "G3cf/VTtAHCdHZNxc5GXWRI8z5P0goL2amXWDVFgb6D3XK/QMtZW90TYdl5zffDCNpiZThJzk0+eEU/Y/aYS6fyIOpQZGFrOr8hmvx5869sl2kr5u8qjnM7q5b4ZnTqdKDLtNxr3Qr7anj6YLpox1FOsiyT26mktXL+7SFOaZ15NMtne1z4xrj4R2SndowI/Znsapo7Gfzvp+L7XJyQ8kLYYRk3INjvmRPPQoJg1R0Nnh6EQE3ldIdwylB7GKtr6a71N/yCd4ZtyIcqq1ZNzdWcZyy5eEBAlDIxuECdBqH6hRq2/RbkfARqidNN4Kq0WviSRaRYGbiNjl2W9pNcM8g=="
+  };
+  const options = {
+    url: '/api/v3/purchases',
+    method: 'POST',
+    baseURL: 'https://payment-service.k8.isw.la',
+    port: 443,
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
+      params: param
+
+    }
+  }
+
+  const makePayment = await axios(options).then(response => {
+    if (response.status == 200) {
+      console.log(response.data)
+    } else {
+      console.log(response.status)
+    }
+  }).catch(error => {
+    return error
+  })
+
 }
+
