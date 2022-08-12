65af9596fc8e50529b022013cb9c8f7ab06ed60f
Nobert Jakpor <jakpornobert@gmail.com>
2022-08-12 12:26:37 +0100

    created exports

--- interswitchcontrollers.js
+++ interswitchcontrollers.js
@@ -2,6 +2,53 @@ const axios = require("axios")
 const dotenv = require("dotenv")
 const forge = require("node-forge")
 
+require("dotenv").config()
+
+
+const getAuthData = function(options){
+    var authString = "1Z"+options.card + 'Z' + options.pin + 'Z' + options.exp + 'Z' + options.cvv;
+    //console.log("Auth-string: "+authString);
+    var vv = toHex(authString);
+    //var vv = SecureManager.toHex(options.authData);
+    //console.log("vv: "+vv);
+    var authDataBytes = forge.util.hexToBytes(vv);
+    var clearSecureBytes = forge.util.createBuffer();
+
+    var rsa = forge.pki.rsa;
+    var modulos = new forge.jsbn.BigInteger(options.publicKeyModulus, 16);
+    var exp = new forge.jsbn.BigInteger(options.publicKeyExponent, 16);
+    var publicKey = rsa.setPublicKey(modulos, exp);
+
+    var pexp = new forge.jsbn.BigInteger('4913cc0183c7a4a74b5405db55a15db8942f38c8cd7974b3644f6b625d22451e917345baa9750be9f8d10da47dbb45e602c86a6aa8bc1e7f7959561dbaaf35e78a8391009c8d86ee11da206f1ca190491bd765f04953765a2e55010d776044cb2716aee6b6f2f1dc38fce7ab0f4eafec8903a73555b4cf74de1a6bfc7f6a39a869838e3678dcbb96709068358621abf988e8049d5c07d128c5803e9502c05c3e38f94658480621a3e1c75fb4e39773e6eec50f5ef62958df864874ef0b00a0fb86f8382d1657381bc3c283567927f1f68d60205fd7ca1197265dd85c173badc1a15044f782602a9e14adc56728929c646c24fe8e10d26afc733158841d9ed4d1', 16);
+    var privateKey = rsa.setPrivateKey(modulos, pexp);
+
+    clearSecureBytes.putBytes(authDataBytes);
+    var vvvv = clearSecureBytes.getBytes();
+
+    // console.log("Clear secure: "+forge.util.bytesToHex(vvvv));
+
+    var authBytes = publicKey.encrypt(vvvv);
+    var auth = forge.util.encode64(authBytes);
+    //console.log("Auth-hex: "+auth);
+
+    //var dauth = privateKey.decrypt(auth, 'RSAES-PKCS1-V1_5');
+    //console.log("dauth-hex: "+dauth);
+
+    return auth;
+}
+
+const toHex = function(str){
+
+    var hex = '';
+    for(var i=0;i<str.length;i++) {
+        hex += ''+str.charCodeAt(i).toString(16);
+    }
+    return hex;
+
+};
+
+module.exports = authData
+
 const generateToken = async () => {
   const options = {
     method: 'POST',
@@ -13,7 +60,7 @@ const generateToken = async () => {
 
   const url = 'https://apps.qa.interswitchng.com/passport/oauth/token?grant_type=client_credentials'
 
-  const token = await axios(url, options).then(response => {
+  return await axios(url, options).then(response => {
     console.log(response.data)
 
     if (response.status == 200) {
@@ -35,24 +82,20 @@ const authOTP = async (paymentID, OTP, token) => {
 
   const url = 'https://payment-service.k8.isw.la/api/v3/purchases/otps/auths'
 
-  const authenticate = await axios(url, options).then(response => {
+  return await axios(url, options).then(response => {
     console.log(response.data.responseCode)
   })
-  
-  fetch('https://payment-service.k8.isw.la/api/v3/purchases/otps/auths', options)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
 }
 
-const makePayment = async (amount, token) => {
+const makePayment = async (amount, token, auth_) => {
   const param = {
     customerId: "1407002510",
     amount: JSON.stringify(amount),
     transactionRef: "12n345mmm0km655",
     currency: "NGN",
-    authData: "G3cf/VTtAHCdHZNxc5GXWRI8z5P0goL2amXWDVFgb6D3XK/QMtZW90TYdl5zffDCNpiZThJzk0+eEU/Y/aYS6fyIOpQZGFrOr8hmvx5869sl2kr5u8qjnM7q5b4ZnTqdKDLtNxr3Qr7anj6YLpox1FOsiyT26mktXL+7SFOaZ15NMtne1z4xrj4R2SndowI/Znsapo7Gfzvp+L7XJyQ8kLYYRk3INjvmRPPQoJg1R0Nnh6EQE3ldIdwylB7GKtr6a71N/yCd4ZtyIcqq1ZNzdWcZyy5eEBAlDIxuECdBqH6hRq2/RbkfARqidNN4Kq0WviSRaRYGbiNjl2W9pNcM8g=="
+    authData: auth_
   };
+
   const options = {
     url: '/api/v3/purchases',
     method: 'POST',
@@ -66,7 +109,7 @@ const makePayment = async (amount, token) => {
     }
   }
 
-  const makePayment = await axios(options).then(response => {
+  return await axios(options).then(response => {
     if (response.status == 200) {
       console.log(response.data)
       console.log(response.data.responseCode)
@@ -91,8 +134,15 @@ const confirmTransaction = async () => {
 
   const url = 'https://qa.interswitchng.com/collections/api/v1/gettransaction.json?merchantCode=MX6072&transactionReference=123456xx1x&merchantcode=merchantcode&transactionreference=reference&amount=amount%20%2F'
 
-  const status = await axios(url, options).then(response => {
+  return await axios(url, options).then(response => {
     console.log(response.data)
   })
 }
 
+module.exports = {
+  getAuthData,
+  authOTP,
+  generateToken,
+  makePayment,
+  confirmTransaction
+}
